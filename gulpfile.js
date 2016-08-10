8902c245736afbf978a63096e43338166985eca1
Nathan-Bateman <nathan.bateman.jr@gmail.com>
2016-08-10 12:01:47 -0500

    added form fields to markup

--- gulpfile.js
+++ gulpfile.js
@@ -17,13 +17,13 @@ var gulp = require('gulp'),
 //Scripts Task
 //////////////// Grabs all .js files and pipes them out to a destination
 gulp.task('concatscripts', function(){
-	return gulp.src([ 'js/jQuery.js','js/knockout-3.3.0.js','js/bootstrap.js' ,'js/main.js'])
+	return gulp.src([ 'js/jQuery.js','js/tether.js','js/knockout-3.3.0.js','js/bootstrap.js' ,'js/main.js'])
 	.pipe(plumber())
     .pipe(concat('final.js'))
 	.pipe(gulp.dest('js'))
 });
 
-gulp.task('scripts', function(){
+gulp.task('uglifyscripts', function(){
 	gulp.src(['js/**/*final.js', '!js/**/*.min.js'])
 	.pipe(plumber())
 	.pipe(rename({suffix:'.min'}))
@@ -38,16 +38,17 @@ gulp.task('scripts', function(){
 //////
 //BROWSER-SYNC
 ////////////////
-gulp.task('serve', ['sass'], function() {
+gulp.task('watch', ['sass'], function() {
 
-    browserSync.init({
-        server: "./"
-    });
+    // browserSync.init({
+    //     server: "./"
+    // });
     gulp.watch('js/**/*.js', ['concatscripts']);
+    gulp.watch('js/**/*final.js', ['uglifyscripts']);
 	gulp.watch('scss/**/*.scss',['sass']);
 	gulp.watch('css/**/*.css', ['concatcss']);
 	gulp.watch('css/**/*.css', ['minifycss']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+    // gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 /////
@@ -120,15 +121,15 @@ gulp.task('minifyhtml', function() {
 ////////////////
 //////
 
-gulp.task('watch', function(){
-	gulp.watch('js/**/*.js', ['concatscripts']);
-	gulp.watch('scss/**/*.scss',['sass']);
-	gulp.watch('css/**/*.css', ['concatcss']);
-	gulp.watch('css/**/*.css', ['minifycss']);
+// gulp.task('watch', function(){
+// 	gulp.watch('js/**/*.js', ['concatscripts']);
+// 	gulp.watch('scss/**/*.scss',['sass']);
+// 	gulp.watch('css/**/*.css', ['concatcss']);
+// 	gulp.watch('css/**/*.css', ['minifycss']);
 
-});
+// });
 
 
 //Default Task
 ////////////////
-gulp.task('default', ['serve']);
+gulp.task('default', ['watch']);
